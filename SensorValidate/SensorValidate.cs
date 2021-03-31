35b396d8d48da7d07340079c4002692a65446929
RashmitaSoni <36473003+RashmitaSoni@users.noreply.github.com>
2021-03-31 09:49:57 +0530

    Update SensorValidate.cs

--- SensorValidate/SensorValidate.cs
+++ SensorValidate/SensorValidate.cs
@@ -18,16 +18,16 @@ namespace SensorValidate
             int lastButOneIndex = values.Count - 1;
             for (int i = 0; i < lastButOneIndex; i++)
             {
-                if (!IsValidSensorMeasureReading(values[i], values[i + 1], maxDelta))
+                if (IsDifferenceInValueGreaterThanMaxdelta(values[i], values[i + 1], maxDelta))
                 {
                     return false;
                 }
             }
             return true;
         }
-        public static bool IsValidSensorMeasureReading(double value, double nextValue, double maxDelta)
+        public static bool IsDifferenceInValueGreaterThanMaxdelta(double value, double nextValue, double maxDelta)
         {
-            return (nextValue - value > maxDelta) ? false : true;
+            return (nextValue - value > maxDelta) ? true : false;
         }
         public static bool IsSensorReadingsListEmpty(List<Double> values)
         {
